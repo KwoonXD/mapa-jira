c1b9e487e1bea32cf3b5fb0f2ba04ab49b1adef8
KwoonXD <160345147+KwoonXD@users.noreply.github.com>
2025-06-20 15:13:07 -0300

    Update jira_api.py

--- utils/jira_api.py
+++ utils/jira_api.py
@@ -1,19 +1,25 @@
 import requests
-from requests.auth import HTTPBasicAuth
 
 class JiraAPI:
-    def __init__(self, email, api_token, domain):
-        self.auth = HTTPBasicAuth(email, api_token)
-        self.domain = domain
-        self.headers = {"Accept": "application/json", "Content-Type": "application/json"}
+    def __init__(self, email, token, url):
+        self.auth = (email, token)
+        self.url = url.rstrip("/") + "/rest/api/3"
 
     def buscar_chamados(self, jql, fields="summary"):
-        url = f"{self.domain}/rest/api/3/search"
-        params = {
-            "jql": jql,
-            "fields": fields,
-            "maxResults": 1000
-        }
-        response = requests.get(url, headers=self.headers, auth=self.auth, params=params)
-        response.raise_for_status()
-        return response.json()["issues"]
+        chamados = []
+        start = 0
+        while True:
+            params = {
+                "jql": jql,
+                "fields": fields,
+                "startAt": start,
+                "maxResults": 100
+            }
+            resp = requests.get(f"{self.url}/search", auth=self.auth, params=params)
+            resp.raise_for_status()
+            data = resp.json()
+            chamados += data.get("issues", [])
+            if start + 100 >= data.get("total", 0):
+                break
+            start += 100
+        return chamados
